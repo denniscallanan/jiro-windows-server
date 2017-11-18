18b5402caa3a50e85f2ab58657976674fae061af
swssqewe12 <david@callanan.ie>
2017-11-18 21:32:07 +0000

    Connections done!

--- testing2/bs.py
+++ testing2/bs.py
@@ -9,7 +9,7 @@ UNIQUE_BROADCAST_PORT = int(IPV4.split(".")[3])
 DATA = 0 * 64
 CONNECT = 1 * 64
 DISCONNECT = 2 * 64
-OTHER = 3 * 64
+RECONNECT = 3 * 64
 NO_EXTRA = 0
 
 class Client:
@@ -28,13 +28,18 @@ class Client:
     def _ready_func(self):
         pass
 
+    def _ondisconnect_func(self):
+        pass
+
     def _onmessage(self, event):
         if event.type == CONNECT:
             broadcastport = int(event.msg)
             self.broadcastListener = Listener(BROADCAST, broadcastport)
             self.broadcastListener.onmessage(self._onmessage)
             self._ready_func()
-        else:
+        elif event.type == DISCONNECT:
+            self._ondisconnect_func()
+        elif event.type == DATA:
             self._onmessage_func(event)
 
     def onmessage(self, func):
@@ -43,28 +48,71 @@ class Client:
     def onconnect(self, func):
         self._ready_func = func
 
+    def ondisconnect(self, func):
+        self._ondisconnect_func = func
+
     def send(self, msg, typ=DATA, extra=NO_EXTRA):
         self.sender.send(msg, self.serveraddr, typ, extra)
 
+    def reconnect(self):
+        self.send("", typ=RECONNECT)
+
 class Server:
     def __init__(self, serverport):
+        self.clients = {}
         self.serverport = serverport
         self.listener = Listener(HERE, serverport)
         self.sender = Sender()
 
         self.listener.onmessage(self._onmessage)
 
+        thread.start_new_thread(self.dc_timer, ())
+
+    def dc_timer(self):
+        while True:
+            time.sleep(1)
+
+            to_delete = []
+            
+            for client in self.clients:
+                self.clients[client] -= 1
+                if self.clients[client] == 7:
+                    self.send("", client, typ=DISCONNECT)
+                if self.clients[client] <= 0:
+                    to_delete.append(client)
+                    self._onclientleave_func(obj(addr=client))
+
+            for client in to_delete: del self.clients[client]
+
     def _onmessage_func(self, event):
+        pass
+
+    def _onclientjoin_func(self, event):
+        pass
+
+    def _onclientleave_func(self, event):
         pass
 
     def _onmessage(self, event):
         if event.type == CONNECT:
             self.send(str(self.serverport + UNIQUE_BROADCAST_PORT), event.addr, typ=CONNECT)
-        else:
+            self._onclientjoin_func(obj(addr=event.addr))
+        elif event.type == DISCONNECT:
+            del self.clients[event.addr]
+            self._onclientleave_func(obj(addr=event.addr))
+        elif event.type == DATA:
             self._onmessage_func(event)
+
+        self.clients[event.addr] = 17
 
     def onmessage(self, func):
         self._onmessage_func = func
+
+    def onclientjoin(self, func):
+        self._onclientjoin_func = func
+
+    def onclientleave(self, func):
+        self._onclientleave_func = func
 
     def send(self, msg, addr, typ=DATA, extra=NO_EXTRA):
         self.sender.send(msg, addr, typ, extra)
@@ -104,7 +152,7 @@ class Sender:
         self.socket.bind(('', 0))
 
     def send(self, msg, addr, typ=DATA, extra=NO_EXTRA):
-        data = chr(min(max(typ,0),OTHER) + min(max(extra,0),63)) + msg
+        data = chr(min(max(typ,0),RECONNECT) + min(max(extra,0),63)) + msg
         if addr[0] == BROADCAST: self.socket.setsockopt(s.SOL_SOCKET, s.SO_BROADCAST, 1)
         self.socket.sendto(data, addr)
         if addr[0] == BROADCAST: self.socket.setsockopt(s.SOL_SOCKET, s.SO_BROADCAST, 0)
--- testing2/server.py
+++ testing2/server.py
@@ -5,8 +5,16 @@ def message(event):
     if event.msg == "Hello, Server!":
         socket.send("Hello, Client!", ("localhost", event.addr[1]))
 
+def clientjoin(event):
+    print "Client connected -",event.addr
+
+def clientleave(event):
+    print "Client disconnected -",event.addr
+
 socket = bs.Server(36883)
 socket.onmessage(message)
+socket.onclientjoin(clientjoin)
+socket.onclientleave(clientleave)
 
 print "Serving on port 36883..."
 
