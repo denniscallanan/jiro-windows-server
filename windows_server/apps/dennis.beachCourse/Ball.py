5963851b226b92f2311716fd30a6e06a32ae5196
swssqewe12 <david@callanan.ie>
2018-03-04 17:28:30 +0000

    adding code for squishing ball that doesn't work

--- windows_server/apps/dennis.beachCourse/Ball.py
+++ windows_server/apps/dennis.beachCourse/Ball.py
@@ -16,8 +16,11 @@ class Ball(pyglet.sprite.Sprite, CameraRelativeSprite):
         self.velocity_x = 0
         self.velocity_y = 0
         self.acceleration_x = 0
-        self.gravity = 30
+        self.gravity = -30
         self.attempt_jump = 0
+        self.bounce_state = 0 # 0 = normal, 1 = squishing, 2 = expanding
+        self.HAV = 200 # static constant
+        self.absorbed_vel = 0
 
     def update_acceleration_x(self, x):
         if x > -0.5 and x < 0.5:
@@ -25,8 +28,16 @@ class Ball(pyglet.sprite.Sprite, CameraRelativeSprite):
         self.acceleration_x = x
 
     def update(self, dt):
+        if self.bounce_state == 0: # normal
+            self.update_state_normal(dt)
+        '''elif self.bounce_state == 1: # squishing
+            self.update_state_squishing(dt)
+        elif self.bounce_state == 2: # expanding
+            self.update_state_expanding(dt)'''
+
+    def update_state_normal(self, dt):
         self.velocity_x += self.acceleration_x * dt * 40
-        self.velocity_y -= self.gravity * dt
+        self.velocity_y += self.gravity * dt
         self.velocity_x /= 1 + (0.5 * dt)
         if self.velocity_x > 300:
             self.velocity_x = 300
@@ -36,6 +47,25 @@ class Ball(pyglet.sprite.Sprite, CameraRelativeSprite):
         self.rotation = self.rot - 30
         self.vpos.x += math.radians(self.velocity_x * dt) * self.vwidth()
         self.vpos.y += self.velocity_y
+
+    '''def update_state_squishing(self, dt):
+        print self.velocity_y
+        acceleration_y = -self.gravity * dt
+        self.velocity_y += acceleration_y
+        self.absorbed_vel += 0.8 * acceleration_y
+        self.scale_y = self.HAV / (self.HAV + self.absorbed_vel)
+        if self.velocity_y >= 0:
+            self.velocity_y = 0
+            self.bounce_state = 2 # expanding
+    
+    def update_state_expanding(self, dt):
+        change = -self.gravity * dt
+        self.absorbed_vel -= change
+        self.velocity_y += change
+        self.scale_y = self.HAV / (self.HAV + self.absorbed_vel)
+        if self.absorbed_vel <= 0:
+            self.absorbed_vel = 0
+            self.bounce_state = 0 # normal'''
 
     def check_platform_collisions(self, platforms):
         for platform in platforms:
@@ -62,6 +92,7 @@ class Ball(pyglet.sprite.Sprite, CameraRelativeSprite):
                     self.attempt_jump = 0
             if ball_point.y >= platform.vpos.y - half_plat_size.y and ball_point.y <= platform.vpos.y + half_plat_size.y: # y colliding
                 self.vpos.y += (platform.vpos.y - dir * half_plat_size.y) - (ball_point.y)
+                #self.bounce_state = 1 # squishing
                 self.velocity_y /= -1.2
                 self.update_jump()
                 return True
